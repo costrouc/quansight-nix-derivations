98ca5e21611bf360cfc85cc7d7223953d23abe21
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-11-27 20:54:27 -0500

    Adding uarray-docs build derivation

--- pkgs/uarray.nix
+++ pkgs/uarray.nix
@@ -32,6 +32,26 @@ rec {
     };
   };
 
+  uarray-docs = pkgs.stdenv.mkDerivation {
+    name = "uarray-docs";
+
+    src = with builtins; filterSource
+          (path: _:
+             !elem (baseNameOf path) [".git"])
+          ../../uarray-docs;
+
+    buildInputs = [ pkgs.texlive.combined.scheme-full ];
+
+    buildPhase = ''
+      make
+    '';
+
+    installPhase = ''
+      mkdir -p $out
+      cp build/*.pdf $out
+    '';
+  };
+
   uarray-notebooks = pkgs.mkShell {
     buildInputs = with pythonPackages; [
       black pylint jupyterlab pudb flake8 perf altair pandas # pandoc pypandoc # dev-requirements.txt
