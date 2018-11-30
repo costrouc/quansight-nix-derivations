f3f0cf60a50976eff277a16ac0ad5254bc4d8d29
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-11-29 20:43:19 -0500

    Starting using xndtools to generate `xnd_structinfo.c`

--- pkgs/numba-xnd/python.nix
+++ pkgs/numba-xnd/python.nix
@@ -6,6 +6,7 @@
 , libndtypes
 , libxnd
 , libgumath
+, xndtools
 }:
 
 pythonPackages.buildPythonPackage rec {
@@ -16,6 +17,7 @@ pythonPackages.buildPythonPackage rec {
          !elem (baseNameOf path) [".git"])
       ../../../numba-xnd;
 
+  buildInputs = [ xndtools ];
   propagatedBuildInputs = [ pythonPackages.numpy pythonPackages.llvmlite pythonPackages.argparse xnd gumath ]
      ++ stdenv.lib.optional (!pythonPackages.isPy3k) pythonPackages.funcsigs
      ++ stdenv.lib.optional (pythonPackages.isPy27 || pythonPackages.isPy33) pythonPackages.singledispatch;
@@ -26,6 +28,11 @@ pythonPackages.buildPythonPackage rec {
       'include_dirs = ["${libndtypes}/include", "${libxnd}/include", "${libgumath}/include", "${ndtypes}/include/ndtypes", "${xnd}/include/xnd", "${gumath}/include/gumath", "${pythonPackages.numba}/${pythonPackages.python.sitePackages}/numba/runtime"]' \
       --replace 'library_dirs = [site_packages[: site_packages.find("/python")]]' \
                 'library_dirs = [ "${libndtypes}/lib", "${libxnd}/lib", "${libgumath}/lib" ]' \
+  '';
+
+  preBuild = ''
+    # generate xnd_structinfo.c
+    structinfo_generator structinfo_config.py
   '';
 
   meta = with stdenv.lib; {
